385415e48693977b94aa3c4190ee75630a378d8f
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-04-24 21:35:53 -0700

    add fk to event

--- base/models.py
+++ base/models.py
@@ -3,12 +3,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 import datetime
 
-class Color(models.Model):
-    name = models.CharField(max_length=20)
-    pastel = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -44,9 +38,17 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
+    
+class Color(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+    pastel = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+        
 class Event(models.Model):
     color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True)
-    userId = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    userId = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     start = models.IntegerField(validators=[MaxValueValidator(96), MinValueValidator(0)])
     end = models.IntegerField(validators=[MaxValueValidator(96), MinValueValidator(0)])
