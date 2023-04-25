f3a2a2b380ae6ac275ed91cfa92668295481a92d
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-04-24 23:40:13 -0700

    changed event model and event view

--- base/models.py
+++ base/models.py
@@ -29,27 +29,25 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=50, unique=True)
     username = models.CharField(max_length=30, default='')
     password = models.CharField(max_length=20)
-    bio = models.CharField(max_length=250)
-    age = models.IntegerField(null=True)
     is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['bio']
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
-    
+
 class Color(models.Model):
     name = models.CharField(max_length=20, primary_key=True)
     pastel = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-        
+    
 class Event(models.Model):
     color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True)
     userId = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
-    start = models.IntegerField(validators=[MaxValueValidator(96), MinValueValidator(0)])
-    end = models.IntegerField(validators=[MaxValueValidator(96), MinValueValidator(0)])
+    start = models.IntegerField(validators=[MaxValueValidator(95), MinValueValidator(0)])
+    end = models.IntegerField(validators=[MaxValueValidator(96), MinValueValidator(1)])
     date = models.DateField(default=datetime.date.today)
--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from base.models import Event
 from base.serializers import EventSerializer
@@ -9,7 +11,9 @@ from .tasks import send_the_email
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
     
 class EventView(APIView):
-    
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
     def get(self, request, pk=None):
         if pk is not None:
             event = Event.objects.get(pk=pk)
@@ -18,14 +22,31 @@ class EventView(APIView):
                 return Response(eventSerializer.errors, status=status.HTTP_404_NOT_FOUND)
             return Response(eventSerializer.data)
         
-        events = Event.objects.all()
+        year = request.GET.get('year')
+        month = request.GET.get('month')
+        day = request.GET.get('day')
+        userID = request.GET.get('userId')
+
+        events = Event.objects.filter(date__year=year, date__month=month, userId=userID)
         eventSerializer = EventSerializer(events, many=True)
+
+        for date in eventSerializer.data:
+            if str(date['date']) == f"{year}-{month}-{day}":
+                date['requested_day'] = True
+            else:
+                date['requested_day'] = False
+        
         return Response(eventSerializer.data)
     
     def post(self, request):
         eventSerializer = EventSerializer(data=request.data)
 
         if eventSerializer.is_valid():
+            event = eventSerializer.validated_data
+            
+            if event.get('start') >= event.get('end'):
+                event['end'] = event.get('start') + 1
+
             eventSerializer.save()
             return Response(eventSerializer.data, status=status.HTTP_201_CREATED)
         
