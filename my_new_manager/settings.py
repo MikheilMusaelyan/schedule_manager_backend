6121c254e665c4a376b9b5ac85254d4c52f8dd05
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-05-12 21:29:45 -0700

    allow localhost

--- my_new_manager/settings.py
+++ my_new_manager/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://localhost:4200']
 
 
 # Application definition
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 
 # CORS_ALLOW_METHODS = (
 #     'DELETE',
