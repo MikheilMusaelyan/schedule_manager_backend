fa50f4e16219117d57e4f4230194977d1ce7c24b
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-05-21 13:04:21 -0700

    add static root

--- my_new_manager/settings.py
+++ my_new_manager/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     }
 }
-
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -160,7 +160,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
